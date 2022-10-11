25a2aefbd09d13121a765214d109734588dfea73
Augusto Maia <augustofmaia@hotmail.com>
2022-10-11 15:29:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Testando Endpoint
+app.MapGet("/", () => "Testando a API 22");
+app.MapPost("/", () => new {Name = "Augusto Maia", Age = 36});
+app.MapGet("/AddHeader",(HttpResponse response) => {
+    response.Headers.Add("teste", "Augusto Maia");
+    return new {Name = "Augusto Maia", Age = 36};
+    });
+
+// Passando parâmetros pelo Header, 
+app.MapGet("/getproductbyheader", (HttpRequest request) => {
+    return request.Headers["product-code"].ToString();
+});
+
+// consulta através de Query, usando parâmetros, de forma dinâmica.
+app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
+    return dateStart + " - " + dateEnd;
+});
+
+///////////////////////////////////////////////////
+
+//Passando parâmetro pelo body
+app.MapPost("/products", (Product product) => {
+    ProductReository.Add(product);
+    return Results.Created($"/products/{product.Code}", product.Code);
+});
+
+// por exemplo: api.app.com/user/{code} através da rota.
+app.MapGet("/products/{code}", ([FromRoute]string code) => {
+    var product = ProductReository.GetBy(code);
+    if(product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
+});
+
+// Editando com endpoint do tipo Put
+app.MapPut("/products", (Product product) => {
+    var productSaved = ProductReository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+    return Results.Ok;
+});
+
+// Deletando com endpoint do tipo Delete
+app.MapDelete("/products/{code}", ([FromRoute]string code) => {
+    var productSaved = ProductReository.GetBy(code);
+    ProductReository.Remove(productSaved);
+    return Results.Ok;
+});
+
+app.Run();
+
+public static class ProductReository {
+    public static List<Product> Products { get; set; }
+    public static void Add(Product product){
+        if(Products == null)
+            Products = new List<Product>();
+        
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code) {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product) {
+        Products.Remove(product);
+    }
+
+}
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
